3292003b6d812aaaba3e7f6b9184ce0e2cd23de5
PlayerEG <enderg@qq.com>
2026-06-22 09:09:23 +0800

    release: v0.0.2

--- src/gdpm/__init__.py
+++ src/gdpm/__init__.py
@@ -1,7 +1,7 @@
 """gdpm - Godot Dependency Package Manager."""
 
 __version__ = "0.0.2"
-__tag__ = "beta"
+__tag__ = ""
 
 # Supported tags: "", "dev", "beta", "alpha", "rc"
 # Empty string means stable release
